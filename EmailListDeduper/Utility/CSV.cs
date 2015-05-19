72171b494afd7e570a526abdf7ca4a769305c60a
Ryan Shea <ryansworld10@gmail.com>
2015-05-19 13:34:58 -0700

    Added basic GUI

---
+++ EmailListDeduper/Utility/CSV.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace EmailListDeduper
+{
+	public static class CSV
+	{
+		public static List<Person> ReadPeople(string fileName)
+		{
+			try
+			{
+				using (TextReader textReader = File.OpenText(fileName))
+					using (CsvReader csvReader = new CsvReader(textReader))
+						return csvReader.GetRecords<Person>().ToList();
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+
+		public static bool WritePeople(List<Person> people, string fileName)
+		{
+			try
+			{
+				using (TextWriter textWriter = File.CreateText(fileName))
+					using (CsvWriter csvWriter = new CsvWriter(textWriter))
+						csvWriter.WriteRecords(people);
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+	public sealed class PersonMapping : CsvClassMap<Person>
+	{
+		public PersonMapping()
+		{
+			Map(m => m.Name).Name("Name");
+			Map(m => m.Email).Name("Email");
+		}
+	}
+}
--- EmailListDeduper/View Models/MainViewModel.cs
+++ EmailListDeduper/View Models/MainViewModel.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel.DataAnnotations;
 
 namespace EmailListDeduper
 {
 	public class MainViewModel : ViewModelBase
 	{
+		public ObservableCollection<string> FilesToDedupe { get; set; }
+		public ObservableCollection<string> FilesToCompareAgainst { get; set; }
 
+		[Required]
+		public string OutputFolder
+		{
+			get { return outputFolder; }
+			set
+			{
+				outputFolder = value;
+				RaisePropertyChanged("OutputFolder");
+			}
+		}
+
+		private string outputFolder;
 	}
 }
